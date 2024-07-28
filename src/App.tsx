25e807c64b075ec2eb1608963a360ae2b8f48a3a
jellyyams <53629007+jellyyams@users.noreply.github.com>
2024-07-28 16:17:38 -0700

    switched to vite npm set up

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./styles/App.css";
+import "./fonts/KosugiMaru-Regular.ttf"
+import AboutPanel from "./components/AboutPanel";
+
+function App() {
+    return (
+        <>
+            <AboutPanel />
+        </>
+    );
+}
+
+export default App;
---
+++ src/components/AboutPanel.tsx
@@ -0,0 +1,27 @@
+import '../styles/AboutPanel.css'
+
+export default function AboutPanel() {
+    return (
+        <div className="parentPanel">
+   
+        <div className = "sidePanel">
+            <div className = "about">
+                <h1>KELLY YEN</h1>
+                <p>Hello World! </p>
+                <p>
+                    This is a bunch of text introducing me. I've worked on a variety of projects and
+                    roles etc. etc. blah blah a alskdfjalksdfja lksd wind sdkflalsdlfj asdf
+                </p>
+
+            </div>
+            
+            <div className="socialLinks">
+                <p>github</p>
+                <p>linkedin</p>
+                <p>Bachelor's of Science in Engineering from Franklin W. Olin College of Engineering</p>
+                <p>Master's of Science in Information Networks from Aalto University</p>
+            </div>
+        </div>
+        </div>
+    );
+}

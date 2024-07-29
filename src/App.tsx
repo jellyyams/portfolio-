8b04aae7b50bbc59b72aa231d5ec427cd51e8465
jellyyams <53629007+jellyyams@users.noreply.github.com>
2024-07-28 17:25:22 -0700

    figured out css styling for cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import "./styles/App.css";
 import "./fonts/KosugiMaru-Regular.ttf"
 import AboutPanel from "./components/AboutPanel";
+import ProjectCard from "./components/ProjectCard";
 
 function App() {
     return (
         <>
-            <AboutPanel />
+
+            <div className="parentPanel">
+                <AboutPanel />
+            </div>
+            <div className="cardPanel">
+                <ProjectCard />
+                <ProjectCard />
+                <ProjectCard />
+                <ProjectCard />
+                <ProjectCard />
+            </div>
+ 
+            
         </>
     );
 }
---
+++ src/components/ProjectCard.tsx
@@ -0,0 +1,12 @@
+import "../styles/ProjectCard.css"
+
+export default function ProjectCard() {
+    return(
+        <div className="card">
+            <p>Title of Project</p>
+            <p>Project Management</p>
+            <p>July 2023</p>
+        </div>
+    )
+
+}

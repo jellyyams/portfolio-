ee609d9d162e016fabf4242e3fb7b67459c31d79
jellyyams <53629007+jellyyams@users.noreply.github.com>
2024-07-29 02:28:01 -0700

    styled cards

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,11 +1,41 @@
 import "../styles/ProjectCard.css"
+import { useState } from "react";
+
+
 
 export default function ProjectCard() {
+    // const [hover, setHover] = useState(false);
+    // const onHover = () => {
+    //     setHover(true); 
+    // }
+    // const onHoverOver = () => {
+    //     setHover(false); 
+    // }
+    // const HoverData = "This sentence is a Project descirption, describing what the project is about."
+
     return(
         <div className="card">
-            <p>Title of Project</p>
-            <p>Project Management</p>
-            <p>July 2023</p>
+            <div className = "imgcontainer">
+                <div className="overlay">
+                    Hello There this is the overlay this is a sentence lk odf asldk  wlekr asldkfja sdi welk dilal .
+                </div>
+             
+                <img 
+                    className="image" 
+                    src="src/assets/test.jpeg" 
+                />
+            </div>
+          
+           
+            <div className="cardtext">
+                <h2 className="title">Title of Project</h2>
+                <p className="position">
+                    Project Manager | User Research{"\n\n"}
+                </p>
+
+                <p className="date">July 2023</p>
+
+            </div>
         </div>
     )
 
---
+++ src/components/ProjectPanel.tsx
@@ -0,0 +1,8 @@
+export default function ProjectPanel() {
+    return (
+        <div>
+            <h1>Project Title</h1>
+            <p>Paragraphs of text sldkf asdlkf io sdlka asldkfj asdl;kf ewoi s dflka oiwer lkwej asdflkjodsiouwer laldkfj sdlkkd iiwoier  dkilw </p>
+        </div>
+    )
+}

087e29a8df9e342d1b2c3dbc58216b566e22d8e4
jellyyams <53629007+jellyyams@users.noreply.github.com>
2024-09-17 16:41:28 +0300

    changed about text

--- src/components/AboutPanel.tsx
+++ src/components/AboutPanel.tsx
@@ -12,14 +12,13 @@ export default function AboutPanel({ onClickHandler }: AboutPanelProps) {
             </h1>
             <div className="about">
                 <p>
-                    Hello World! I'm a software developer, academic in training, and aspiring social
-                    scientist passionate about understanding the systemic factors that influence
+                    Hello! I'm a software developer and academic (in training) passionate about understanding the systemic factors that influence
                     technology development.
                 </p>
                 <p></p>
                 <p>
                     I'm currently looking for research positions and full time oppourtunities for Summer of 2025. Feel free
-                    to reach out via Linkedin or email (kelly.sy.yen@gmail.com)
+                    to reach out via <a href="https://www.linkedin.com/in/kelly-yen/">Linkedin</a> or email (kelly.sy.yen@gmail.com)
                 </p>
                 <br></br>
                 <a className="resume" href="YenResume2024.pdf" target="_blank">
@@ -29,17 +28,17 @@ export default function AboutPanel({ onClickHandler }: AboutPanelProps) {
 
             <div className="socialLinks">
                 <p>
-                    <a className = "atag" href="https://github.com/jellyyams">Github</a>
+                    <a href="https://github.com/jellyyams">Github</a>
                 </p>
                 <p>
-                    <a className = "atag" href="https://www.linkedin.com/in/kelly-yen/">Linkedin</a>
+                    <a href="https://www.linkedin.com/in/kelly-yen/">Linkedin</a>
                 </p>
 
                 <p>
-                    Bachelor's of Science in Engineering from Franklin W. Olin College of
+                    Bachelor's of Science in Engineering at Franklin W. Olin College of
                     Engineering (2023)
                 </p>
-                <p>Master's of Science in Information Networks from Aalto University (2026)</p>
+                <p>Master's of Science in Information Networks at Aalto University (2026)</p>
             </div>
         </div>
     );
